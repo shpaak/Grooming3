20432c8be5c68f8f9499b3ec5f4948f90d4f870e
Alexey Pipin <nievzhno@mail.ru>
2024-03-01 00:56:20 +0300

    (づ ◕‿◕ )づ

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Grooming3"
+include(":app")
+ 
